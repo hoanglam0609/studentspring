0d86242f89408f421d93ede5a9a5f9feefceecb9
hoanglam0609 <77632896+hoanglam0609@users.noreply.github.com>
2021-09-15 08:15:33 +0700

    first commit master branch

---
+++ src/main/java/com/hoanglam/studentspringjpa/StudentSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hoanglam.studentspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hoanglam/studentspringjpa/entity/Student.java
@@ -0,0 +1,94 @@
+package com.hoanglam.studentspringjpa.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "job")
+    private String job;
+
+    public Student(int id, String name, int age, String phone, String address, String job) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.address = address;
+        this.job = job;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", job='" + job + '\'' +
+                '}';
+    }
+}
